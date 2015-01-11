133b90873bf4b9b643ebc75db69341d3fe91f08c
LuckyWh <wenhao19920612@163.com>
2015-01-11 12:40:11 +0800

    add masteractivity

---
+++ GitTestOne/src/com/example/gittestone/MasterActivity.java
@@ -0,0 +1,13 @@
+package com.example.gittestone;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MasterActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+	}
+}
